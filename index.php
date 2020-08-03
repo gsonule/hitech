208500e3cbe7ec2a912973ea53d0055926443e98
gsonule <43131878+gsonule@users.noreply.github.com>
2020-08-03 18:36:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+include_once("header.php");
+?>
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center">
+						<div class="banner-content col-lg-12 col-md-12">
+							<h6 class="text-uppercase">Don’t look further, here is the key</h6>
+							<h1>
+								We’re Industrial solution			
+							</h1>
+							<p class="text-white">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et <br> dolore magna aliqua. Ut enim ad minim.  sed do eiusmod tempor incididunt.
+							</p>
+							<!-- <a href="#" class="primary-btn header-btn text-uppercase">Get Started</a> -->
+						</div>												
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+			<!-- Start cat Area -->
+			<section class="cat-area section-gap" id="feature">
+				<div class="container">							
+					<div class="row">
+						<div class="col-lg-6">	
+							<div class="single-cat d-flex flex-column">
+								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-magic-wand"></span></span></a>
+								<h4 class="mb-20" style="margin-top: 23px;">Maintenance</h4>
+								<p>
+									We are provide annual maintenace for more details contact us.
+								</p>
+							</div>															
+						</div>
+						<div class="col-lg-6">	
+							<div class="single-cat">
+								<a href="#" class="hb-sm-margin mx-auto d-block"><span class="hb hb-sm inv hb-facebook-inv"><span class="lnr lnr-rocket"></span></span></a>
+								<h4 class="mt-40 mb-20">Service</h4>
+								<p>
+									We are providing 24X7 days Service on sales products.	
+								</p>
+							</div>															
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End cat Area -->		
+			
+<?php
+
+include_once("footer.php");
+?>
+
+
